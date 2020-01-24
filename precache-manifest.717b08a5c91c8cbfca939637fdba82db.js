868c7a5077ffec9a62a9082c56d9248d02a789e1
J.M. Alessi <j.m.alessi1@outlook.com>
2020-01-23 21:31:22 -0500

    Updates

---
+++ precache-manifest.717b08a5c91c8cbfca939637fdba82db.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb6b3e4fda5e51f1766eabffc0d45f3b",
+    "url": "/sw-react-app/index.html"
+  },
+  {
+    "revision": "419d12a7b821f52bc2e4",
+    "url": "/sw-react-app/static/css/2.3dda6777.chunk.css"
+  },
+  {
+    "revision": "05e7f17beb1b04eb487d",
+    "url": "/sw-react-app/static/css/main.d1d0e728.chunk.css"
+  },
+  {
+    "revision": "419d12a7b821f52bc2e4",
+    "url": "/sw-react-app/static/js/2.23f8bb69.chunk.js"
+  },
+  {
+    "revision": "f8513fb9f2981a1b206fb733b985f272",
+    "url": "/sw-react-app/static/js/2.23f8bb69.chunk.js.LICENSE"
+  },
+  {
+    "revision": "05e7f17beb1b04eb487d",
+    "url": "/sw-react-app/static/js/main.116cb560.chunk.js"
+  },
+  {
+    "revision": "1b04a285b0a2ae55b665",
+    "url": "/sw-react-app/static/js/runtime-main.026b4137.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/sw-react-app/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/sw-react-app/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/sw-react-app/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/sw-react-app/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/sw-react-app/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/sw-react-app/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/sw-react-app/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/sw-react-app/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/sw-react-app/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/sw-react-app/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/sw-react-app/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/sw-react-app/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/sw-react-app/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/sw-react-app/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/sw-react-app/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/sw-react-app/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
